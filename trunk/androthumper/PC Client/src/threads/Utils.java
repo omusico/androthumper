bc19b3df78459933432c603ac53d79ab320fe270
Flynny75@gmail.com <Flynny75@gmail.com@14bd5244-ea9f-deb6-cc38-107438cbdefd>
2012-04-11 14:45:06 +0000

    Changed utils transmission to TCP. Allows both server and client to know when the connection has dropped, and is more reliable than UDP

--- trunk/androthumper/PC Client/src/threads/Utils.java
+++ trunk/androthumper/PC Client/src/threads/Utils.java
@@ -1,11 +1,11 @@
 package threads;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -24,42 +24,36 @@ import ui.Window;
  */
 public class Utils{
 
-	/**Flag to show if we have received ping from client. */
-	private boolean recievedPing = false;
 	/**Flag to signify if the threads are allowed to run. */
 	private boolean running = true;
+	/**Flag to signify if the socket is still connected. */
+	private boolean stillConnected = false;
 	/**Thread for listening to the {@link #listeningSocket}. */
 	private Thread listeningThread;
 	/**Thread for sending the packets to the client with the {@link #sendSocket}. */
 	private Thread sendingThread;
-	/**Socket to listen for packets from the client. */
-	private DatagramSocket listeningSocket;
-	/**Socket to send packets to the client. */
-	private DatagramSocket sendSocket;
-	/**A packet to hold the data to send to the client. */
-	private DatagramPacket sendPacket;
-	/**A packet to hold the data recieved from the client. */
-	private DatagramPacket listenPacket;
+	/**Thread to set up the initial connection. */
+	private Thread connectionThread;
 	/**A queue to hold the information to send to he client. */
 	private BlockingQueue<byte[]> sendingQueue;
+	/**The input stream from the socket.*/
+	private InputStream socketInput;
+	/**The output stream to the socket. */
+	private OutputStream socketOutput;
+	
+	private ServerSocket serverSocket;
 
 	public Utils(Window window2){
 		try {
-			listeningSocket = new DatagramSocket(Conts.Ports.UTILS_INCOMMING_PORT,InetAddress.getLocalHost());
-			sendSocket = new DatagramSocket();
-			
 			sendingQueue = new ArrayBlockingQueue<byte[]>(20);
-			sendPacket = new DatagramPacket(new byte[]{1}, 1);
-			listenPacket = new DatagramPacket(new byte[Conts.PacketSize.UTILS_CONTROL_PACKET_SIZE], Conts.PacketSize.UTILS_CONTROL_PACKET_SIZE);
-			
-			listeningThread = new Thread(listenRunnable);
-			listeningThread.start();
-			
-			sendingThread = new Thread(sendRunnable);
-			sendingThread.start();
+			serverSocket = new ServerSocket(Conts.Ports.UTILS_INCOMMING_PORT, 0, InetAddress.getLocalHost());
+			connectionThread = new Thread(connectionRunnable);
+			connectionThread.start();
 		} catch (SocketException e) {
 			e.printStackTrace();
 		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
@@ -70,7 +64,7 @@ public class Utils{
 	 * @param command - the command to send. See {@link Conts} //TODO break into inner classes for types
 	 */
 	public void sendCommand(byte command){
-		if(!isWaitingForPing()){
+		if(isConnected()){
 			byte[] data = new byte[Conts.PacketSize.UTILS_CONTROL_PACKET_SIZE];
 			data[0] = command;
 			sendData(data);
@@ -95,8 +89,8 @@ public class Utils{
 	 * Is the Utils waiting for a ping from the client?
 	 * @return True if so, false otherwise.
 	 */
-	public boolean isWaitingForPing(){
-		return !recievedPing;
+	public boolean isConnected(){
+		return stillConnected;
 	}
 	
 	/**
@@ -117,28 +111,14 @@ public class Utils{
 	private Runnable listenRunnable = new Runnable(){
 		@Override
 		public void run() {
-			while(running){
-				if(!recievedPing){
-					Window.PrintToLog("Utils wait.");
-				}
-				
+			while(running && stillConnected){
 				try {
-					listeningSocket.receive(listenPacket);
-					if(!recievedPing){
-						Window.PrintToLog("Utils got.");
-						byte[] data = listenPacket.getData();
-						ByteArrayInputStream bais = new ByteArrayInputStream(data);
-						DataInputStream dis = new DataInputStream(bais);
-						int port = dis.readInt();
-						dis.close();
-						sendPacket.setPort(port);
-						sendPacket.setAddress(listenPacket.getAddress());
-						recievedPing = true;
-					}
-					
-					processData(listenPacket.getData());
-					
+					byte[] data = new byte[Conts.PacketSize.UTILS_CONTROL_PACKET_SIZE];
+					socketInput.read(data);
+					processData(data);
 				} catch (IOException e) {
+					stillConnected = false;
+					System.out.println("Lost connection.");
 					e.printStackTrace();
 				}
 			}
@@ -151,17 +131,49 @@ public class Utils{
 	private Runnable sendRunnable = new Runnable(){
 		@Override
 		public void run() {
-			while(running){
+			while(running && stillConnected){
 				try {
-					sendPacket.setData(sendingQueue.take());
-					sendSocket.send(sendPacket);
+					socketOutput.write(sendingQueue.take());
 				} catch (InterruptedException e) {
 					e.printStackTrace();
-				} catch (IOException e) {
+				}catch (IOException e) {
+					stillConnected = false;
 					e.printStackTrace();
 				}
 			}
 		}
 	};
+	
+	/**Runnable for {@link #connectionThread}. This accepts the first connection and
+	 * starts listening/sending on it.*/
+	private Runnable connectionRunnable = new Runnable() {
+		@Override
+		public void run() {
+			try {
+				Window.PrintToLog("Utils wait.");
+				startComms(serverSocket.accept());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	};
+	/**Start the communication. Get the streams from the socket and start the threads. */
+	private void startComms(Socket socket){
+		try {
+			socketInput = socket.getInputStream();
+			socketOutput = socket.getOutputStream();
+			
+			stillConnected = true;
+			Window.PrintToLog("Utils connected.");
+			
+			listeningThread = new Thread(listenRunnable);
+			sendingThread = new Thread(sendRunnable);
+			
+			listeningThread.start();
+			sendingThread.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
