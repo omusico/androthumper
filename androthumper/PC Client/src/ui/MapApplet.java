e626355a9993bfa57fb8337ad34fec43d5a12607
Flynny75 <Flynny75@gmail.com>
2012-04-24 13:10:39 +0000

    Can now insert points by clicking two previous points, then a new place

--- androthumper/PC Client/src/ui/MapApplet.java
+++ androthumper/PC Client/src/ui/MapApplet.java
@@ -34,59 +34,61 @@ public class MapApplet extends PApplet{
 	private static final List<Float> DISPLAY_DISTANCES = Arrays.asList(0.01f, 0.02f, 0.05f, 0.1f, 0.2f, 0.5f, 1f, 2f,
 			5f, 10f, 20f, 50f, 100f, 200f, 500f, 1000f, 2000f, 5000f);
 	private static final float MAX_DISPLAY_DISTANCE = 5000;
-	
-	// If false it uses the screen center, resulting in a bar scale depending on the north/south position of the map.
-	boolean showDistanceAtEquator = true;
-	
-	Map map,map1,map2;
-    private float lat = 52.41892f,lng = -4.081652f,accuracy=20;
-    private Location currentLocation;
-    private Vector<Location> points = new Vector<Location>();
-    private Window window;
 
-	public MapApplet(Window window, float lat, float lng, Vector<float[]> points){
-		currentLocation = new Location(lat,lng);
-		this.window = window;
-		
-		for(int i = 0; i < points.size(); i++){
-			float[] pair = points.get(i);
-			this.points.add(new Location(pair[0],pair[1]));
+	// If false it uses the screen center, resulting in a bar scale depending on the north/south position of the map.
+	boolean showDistanceAtEquator = true,pressedOnDot = false;
+
+	Map map,map1,map2;
+	private float lat = 52.41892f,lng = -4.081652f,accuracy=20;
+	private Location currentLocation,pressedLocation;
+	private Vector<Location> points = new Vector<Location>();
+	private Window window;
+	private Location highlightedLocation1, highlightedLocation2;
+
+		public MapApplet(Window window, float lat, float lng, Vector<float[]> points){
+			currentLocation = new Location(lat,lng);
+			this.window = window;
+			
+			for(int i = 0; i < points.size(); i++){
+				float[] pair = points.get(i);
+				this.points.add(new Location(pair[0],pair[1]));
+			}
 		}
-	}
 
 	public void setup() {
 		size(800, 600, GLConstants.GLGRAPHICS);
 		currentLocation = new Location(lat,lng);
 		map1 = new Map(this, new Google.GoogleMapProvider());
 		map2 = new Map(this, new Microsoft.AerialProvider());
-		
+
 		map1.zoomAndPanTo(currentLocation, 15);
 		map2.zoomAndPanTo(currentLocation, 15);
-		
+
 		MapUtils.createDefaultEventDispatcher(this, map1, map2);
-		
+
 		map = map1;
 	}
 
 	public void draw() {
 		background(0);
 		map.draw();
-		
+
 		Location last = null;
 		float[] lastPos = null;
+		fill(0,0,0,255);
 		for(int i = 0; i < points.size(); i++){
 			Location l = points.get(i);
 			float[] screenPos = map.getScreenPositionFromLocation(l);
-			
+
 			if(last != null){
-					line(screenPos[0],screenPos[1],lastPos[0],lastPos[1]);
+				line(screenPos[0],screenPos[1],lastPos[0],lastPos[1]);
 			}
-			
+
 			ellipse(screenPos[0], screenPos[1], 10, 10);
 			last = l;
 			lastPos = screenPos;
 		}
-		
+
 		fill(48,139,206,127);
 		float[] myPos = map.getScreenPositionFromLocation(currentLocation);
 		float zoom = 45000/map.getZoom();
@@ -94,16 +96,27 @@ public class MapApplet extends PApplet{
 		fill(48,139,206,255);
 		zoom = 200000/map.getZoom();
 		ellipse(myPos[0],myPos[1],accuracy/zoom,accuracy/zoom);
+
+		if(highlightedLocation1 != null){
+			fill(27,224,50,255);
+			float[] screenPos = map.getScreenPositionFromLocation(highlightedLocation1);
+			ellipse(screenPos[0], screenPos[1], 10, 10);
+		}
+		if(highlightedLocation2 != null){
+			fill(28,139,206,255);
+			float[] screenPos = map.getScreenPositionFromLocation(highlightedLocation2);
+			ellipse(screenPos[0], screenPos[1], 10, 10);
+		}
 		drawBarScale(20, map.mapDisplay.getHeight() - 20);
 	}
 
 	public void jumpToLocation(float lat, float lng){
 		currentLocation = new Location(lat,lng);
-		
+
 		map1.zoomAndPanTo(currentLocation, 10);
 		map2.zoomAndPanTo(currentLocation, 10);
 	}
-	
+
 	public static void main(String[] args) {
 		PApplet.main(new String[] { "ui.MapApplet" });
 	}
@@ -116,13 +129,83 @@ public class MapApplet extends PApplet{
 			handleRightClick(e.getX(), e.getY());
 		}
 	}
-	
-	private void handleLeftClick(int x, int y){
-		points.add(map.getLocationFromScreenPosition(x, y));
-		if(window != null){
-			window.setPoints(floatsFromLocations());
+
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		if(pressedOnDot){
+			Location newLocation = map.getLocationFromScreenPosition(e.getX(), e.getY());
+			pressedLocation.setLat(newLocation.getLat());
+			pressedLocation.setLon(newLocation.getLon());
+		}else{
+			super.mouseDragged(e);
 		}
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		for(int i = 0; i <points.size(); i++){
+			Location l = points.get(i);
+			float[] screenPos = map.getScreenPositionFromLocation(l);
+			if(dist(screenPos[0],screenPos[1],e.getX(),e.getY()) < 10){
+				pressedOnDot = true;
+				pressedLocation = l;
+				printt("on dot: "+i);
+				return;
+			}
+		}
+		super.mousePressed(e);
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		pressedOnDot = false;
+		super.mouseReleased(e);
+	}
+
+	private void handleLeftClick(int x, int y){
+		if(highlightedLocation1 != null){
+			float[] screenPos = map.getScreenPositionFromLocation(highlightedLocation1);
+			if(dist(screenPos[0],screenPos[1],x,y) < 10){
+				highlightedLocation1 = null;
+				return;
+			}
+		}
+		if(highlightedLocation2 != null){
+			float[] screenPos = map.getScreenPositionFromLocation(highlightedLocation2);
+			if(dist(screenPos[0],screenPos[1],x,y) < 10){
+				highlightedLocation2 = null;
+				return;
+			}
+		}
+
+			for(int i = 0; i <points.size(); i++){
+				Location l = points.get(i);
+				float[] screenPos = map.getScreenPositionFromLocation(l);
+				if(dist(screenPos[0],screenPos[1],x,y) < 10){
+					if(highlightedLocation1 == null){
+						highlightedLocation1 = l;
+					}else if(highlightedLocation2 == null){
+						highlightedLocation2 = l;
+					}else{
+						highlightedLocation1 = null;
+						highlightedLocation2 = null;
+					}
+					return;
+				}
+			}
+
+			if(highlightedLocation1 != null && highlightedLocation2 != null){
+				points.insertElementAt(map.getLocationFromScreenPosition(x, y), points.indexOf(highlightedLocation1));
+				highlightedLocation1 = null;
+				highlightedLocation2 = null;
+			}else{
+				points.add(map.getLocationFromScreenPosition(x, y));
+			}
+			
+			if(window != null){
+				window.setPoints(floatsFromLocations());
+			}
 		
+
 	}
 	private void handleRightClick(int x, int y){
 		for(int i = 0; i <points.size(); i++){
@@ -136,7 +219,7 @@ public class MapApplet extends PApplet{
 			}
 		}
 	}
-	
+
 	public void keyPressed() {
 		if (key == '1') {
 			map = map1;
@@ -144,7 +227,7 @@ public class MapApplet extends PApplet{
 			map = map2;
 		}
 	}
-	
+
 	private Vector<float[]> floatsFromLocations(){
 		Vector<float[]> newPoints = new Vector<float[]>();
 		for(Location l:points){
@@ -153,7 +236,7 @@ public class MapApplet extends PApplet{
 		}
 		return newPoints;
 	}
-	
+
 	/**
 	 * Draws a bar scale at given position according to current zoom level.
 	 * 
@@ -222,153 +305,153 @@ public class MapApplet extends PApplet{
 
 		return closest;
 	}
-	
+
 	public void printt(String print){
 		System.out.println(print);
 	}
-	
+
 	@Override
 	public InputStream createInputRaw(String filename) {
-	    InputStream stream = null;
+		InputStream stream = null;
 
-	    if (filename == null) return null;
+		if (filename == null) return null;
 
-	    if (filename.length() == 0) {
-	      // an error will be called by the parent function
-	      //System.err.println("The filename passed to openStream() was empty.");
-	      return null;
-	    }
+		if (filename.length() == 0) {
+			// an error will be called by the parent function
+			//System.err.println("The filename passed to openStream() was empty.");
+			return null;
+		}
 
-	    // safe to check for this as a url first. this will prevent online
-	    // access logs from being spammed with GET /sketchfolder/http://blahblah
-	    if (filename.indexOf(":") != -1) {  // at least smells like URL
-	      try {
-	        URL url = new URL(filename);
-	        
-	        URLConnection uc = url.openConnection();
-	        uc.addRequestProperty("User-Agent", 
-	        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+		// safe to check for this as a url first. this will prevent online
+		// access logs from being spammed with GET /sketchfolder/http://blahblah
+		if (filename.indexOf(":") != -1) {  // at least smells like URL
+			try {
+				URL url = new URL(filename);
 
-	        uc.connect();
-	       
-	        stream = uc.getInputStream();
-	        return stream;
+				URLConnection uc = url.openConnection();
+				uc.addRequestProperty("User-Agent", 
+						"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
 
-	      } catch (MalformedURLException mfue) {
-	        // not a url, that's fine
+				uc.connect();
 
-	      } catch (FileNotFoundException fnfe) {
-	        // Java 1.5 likes to throw this when URL not available. (fix for 0119)
-	        // http://dev.processing.org/bugs/show_bug.cgi?id=403
+				stream = uc.getInputStream();
+				return stream;
 
-	      } catch (IOException e) {
-	        // changed for 0117, shouldn't be throwing exception
-	        e.printStackTrace();
-	        //System.err.println("Error downloading from URL " + filename);
-	        return null;
-	        //throw new RuntimeException("Error downloading from URL " + filename);
-	      }
-	    }
+			} catch (MalformedURLException mfue) {
+				// not a url, that's fine
 
-	    // Moved this earlier than the getResourceAsStream() checks, because
-	    // calling getResourceAsStream() on a directory lists its contents.
-	    // http://dev.processing.org/bugs/show_bug.cgi?id=716
-	    try {
-	      // First see if it's in a data folder. This may fail by throwing
-	      // a SecurityException. If so, this whole block will be skipped.
-	      File file = new File(dataPath(filename));
-	      if (!file.exists()) {
-	        // next see if it's just in the sketch folder
-	        file = new File(sketchPath, filename);
-	      }
-	      if (file.isDirectory()) {
-	        return null;
-	      }
-	      if (file.exists()) {
-	        try {
-	          // handle case sensitivity check
-	          String filePath = file.getCanonicalPath();
-	          String filenameActual = new File(filePath).getName();
-	          // make sure there isn't a subfolder prepended to the name
-	          String filenameShort = new File(filename).getName();
-	          // if the actual filename is the same, but capitalized
-	          // differently, warn the user.
-	          //if (filenameActual.equalsIgnoreCase(filenameShort) &&
-	          //!filenameActual.equals(filenameShort)) {
-	          if (!filenameActual.equals(filenameShort)) {
-	            throw new RuntimeException("This file is named " +
-	                                       filenameActual + " not " +
-	                                       filename + ". Rename the file " +
-	            "or change your code.");
-	          }
-	        } catch (IOException e) { }
-	      }
+			} catch (FileNotFoundException fnfe) {
+				// Java 1.5 likes to throw this when URL not available. (fix for 0119)
+				// http://dev.processing.org/bugs/show_bug.cgi?id=403
 
-	      // if this file is ok, may as well just load it
-	      stream = new FileInputStream(file);
-	      if (stream != null) return stream;
+			} catch (IOException e) {
+				// changed for 0117, shouldn't be throwing exception
+				e.printStackTrace();
+				//System.err.println("Error downloading from URL " + filename);
+				return null;
+				//throw new RuntimeException("Error downloading from URL " + filename);
+			}
+		}
 
-	      // have to break these out because a general Exception might
-	      // catch the RuntimeException being thrown above
-	    } catch (IOException ioe) {
-	    } catch (SecurityException se) { }
+		// Moved this earlier than the getResourceAsStream() checks, because
+		// calling getResourceAsStream() on a directory lists its contents.
+		// http://dev.processing.org/bugs/show_bug.cgi?id=716
+		try {
+			// First see if it's in a data folder. This may fail by throwing
+			// a SecurityException. If so, this whole block will be skipped.
+			File file = new File(dataPath(filename));
+			if (!file.exists()) {
+				// next see if it's just in the sketch folder
+				file = new File(sketchPath, filename);
+			}
+			if (file.isDirectory()) {
+				return null;
+			}
+			if (file.exists()) {
+				try {
+					// handle case sensitivity check
+					String filePath = file.getCanonicalPath();
+					String filenameActual = new File(filePath).getName();
+					// make sure there isn't a subfolder prepended to the name
+					String filenameShort = new File(filename).getName();
+					// if the actual filename is the same, but capitalized
+					// differently, warn the user.
+					//if (filenameActual.equalsIgnoreCase(filenameShort) &&
+					//!filenameActual.equals(filenameShort)) {
+					if (!filenameActual.equals(filenameShort)) {
+						throw new RuntimeException("This file is named " +
+								filenameActual + " not " +
+								filename + ". Rename the file " +
+								"or change your code.");
+					}
+				} catch (IOException e) { }
+			}
 
-	    // Using getClassLoader() prevents java from converting dots
-	    // to slashes or requiring a slash at the beginning.
-	    // (a slash as a prefix means that it'll load from the root of
-	    // the jar, rather than trying to dig into the package location)
-	    ClassLoader cl = getClass().getClassLoader();
+			// if this file is ok, may as well just load it
+			stream = new FileInputStream(file);
+			if (stream != null) return stream;
 
-	    // by default, data files are exported to the root path of the jar.
-	    // (not the data folder) so check there first.
-	    stream = cl.getResourceAsStream("data/" + filename);
-	    if (stream != null) {
-	      String cn = stream.getClass().getName();
-	      // this is an irritation of sun's java plug-in, which will return
-	      // a non-null stream for an object that doesn't exist. like all good
-	      // things, this is probably introduced in java 1.5. awesome!
-	      // http://dev.processing.org/bugs/show_bug.cgi?id=359
-	      if (!cn.equals("sun.plugin.cache.EmptyInputStream")) {
-	        return stream;
-	      }
-	    }
+			// have to break these out because a general Exception might
+			// catch the RuntimeException being thrown above
+		} catch (IOException ioe) {
+		} catch (SecurityException se) { }
 
-	    // When used with an online script, also need to check without the
-	    // data folder, in case it's not in a subfolder called 'data'.
-	    // http://dev.processing.org/bugs/show_bug.cgi?id=389
-	    stream = cl.getResourceAsStream(filename);
-	    if (stream != null) {
-	      String cn = stream.getClass().getName();
-	      if (!cn.equals("sun.plugin.cache.EmptyInputStream")) {
-	        return stream;
-	      }
-	    }
+		// Using getClassLoader() prevents java from converting dots
+		// to slashes or requiring a slash at the beginning.
+		// (a slash as a prefix means that it'll load from the root of
+		// the jar, rather than trying to dig into the package location)
+		ClassLoader cl = getClass().getClassLoader();
 
-	    try {
-	      // attempt to load from a local file, used when running as
-	      // an application, or as a signed applet
-	      try {  // first try to catch any security exceptions
-	        try {
-	          stream = new FileInputStream(dataPath(filename));
-	          if (stream != null) return stream;
-	        } catch (IOException e2) { }
+		// by default, data files are exported to the root path of the jar.
+		// (not the data folder) so check there first.
+		stream = cl.getResourceAsStream("data/" + filename);
+		if (stream != null) {
+			String cn = stream.getClass().getName();
+			// this is an irritation of sun's java plug-in, which will return
+			// a non-null stream for an object that doesn't exist. like all good
+			// things, this is probably introduced in java 1.5. awesome!
+			// http://dev.processing.org/bugs/show_bug.cgi?id=359
+			if (!cn.equals("sun.plugin.cache.EmptyInputStream")) {
+				return stream;
+			}
+		}
 
-	        try {
-	          stream = new FileInputStream(sketchPath(filename));
-	          if (stream != null) return stream;
-	        } catch (Exception e) { }  // ignored
+		// When used with an online script, also need to check without the
+		// data folder, in case it's not in a subfolder called 'data'.
+		// http://dev.processing.org/bugs/show_bug.cgi?id=389
+		stream = cl.getResourceAsStream(filename);
+		if (stream != null) {
+			String cn = stream.getClass().getName();
+			if (!cn.equals("sun.plugin.cache.EmptyInputStream")) {
+				return stream;
+			}
+		}
 
-	        try {
-	          stream = new FileInputStream(filename);
-	          if (stream != null) return stream;
-	        } catch (IOException e1) { }
+		try {
+			// attempt to load from a local file, used when running as
+			// an application, or as a signed applet
+			try {  // first try to catch any security exceptions
+				try {
+					stream = new FileInputStream(dataPath(filename));
+					if (stream != null) return stream;
+				} catch (IOException e2) { }
 
-	      } catch (SecurityException se) { }  // online, whups
+				try {
+					stream = new FileInputStream(sketchPath(filename));
+					if (stream != null) return stream;
+				} catch (Exception e) { }  // ignored
 
-	    } catch (Exception e) {
-	      //die(e.getMessage(), e);
-	      e.printStackTrace();
-	    }
-	    return null;
-	  }
+				try {
+					stream = new FileInputStream(filename);
+					if (stream != null) return stream;
+				} catch (IOException e1) { }
+
+			} catch (SecurityException se) { }  // online, whups
+
+		} catch (Exception e) {
+			//die(e.getMessage(), e);
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
